9dee7d4b2137d37b834d3a167df2e1ad6b2f9dd4
Brian James <your-email@example.com>
2026-05-06 22:47:26 +0300

    fix next config for production build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    // This tells Next.js to stop being so strict while we are building
-    allowedDevOrigins: ["*"], 
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
